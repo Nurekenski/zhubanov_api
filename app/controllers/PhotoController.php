b5f6d17d68c27be55794581e331b46fc2d2acc6a
tima <tima.98.98@bk.ru>
2018-09-20 17:34:03 +0500

    avatar

---
+++ app/controllers/PhotoController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 020 20.09.18
+ * Time: 16:16
+ */
+
+namespace Controllers;
+
+
+class PhotoController
+{
+
+}
---
+++ app/lib/Photo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 020 20.09.18
+ * Time: 15:52
+ */
+
+namespace Lib;
+
+
+class Photo
+{
+
+}

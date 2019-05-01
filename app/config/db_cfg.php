23b818122927f108ee8704c577fd673c127e8f29
Nurbol <nurbolmaden@gmail.com>
2019-05-01 23:17:39 +0600

    changes in db

--- app/config/db_cfg.php
+++ app/config/db_cfg.php
@@ -12,8 +12,13 @@ if (APP_ENV) {
 }
 
 return [
-    'db_host' => 'localhost', //mysql_db
-    'db_name' => 'data',
-    'db_user' => 'root',
-    'db_password' => ''
+    // 'db_host' => 'localhost', //mysql_db
+    // 'db_name' => 'data',
+    // 'db_user' => 'root',
+    // 'db_password' => ''
+    'db_host' => 'icopoghru9oezxh8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', //mysql_db
+    'db_name' => 'x5ccvtlbghqbj0d5',
+    'db_user' => 'vrb92ie5xi4qhz6s',
+    'db_password' => 'huoot8y78297oafx'
+
 ];
